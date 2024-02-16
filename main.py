4f0d7337e00846b03c7134dee7c442f8ced234f5
davidmunkacsi <munkacsi.david@madis.hu>
2024-02-16 15:26:25 +0100

    Add additional functionality to binance futures client.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import pprint
 import tkinter as tk
 import logging
-from bitmex_futures import get_contracts
+from connectors.binance_futures import BinanceFuturesClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -20,25 +21,10 @@ logger.addHandler(fileHandler)
 
 
 if __name__ == '__main__':
-    bitmex_contracts = get_contracts()
+    binance = BinanceFuturesClient(False)
+
+    pprint.pprint(binance.get_historical_candles("BTCUSDT", "1h"))
 
     root = tk.Tk()
-    root.configure(bg="gray12")
-
-    i = 0
-    j = 0
-
-    calibri_font = ("Calibri", 11, "normal")
-
-    for contract in bitmex_contracts:
-        label_widget = tk.Label(root, text=contract, bg='gray12', fg='SteelBlue1', width=13, font=calibri_font)
-        label_widget.grid(row=i, column=j, sticky="ew")
-
-        if i == 4:
-            j += 1
-            i = 0
-
-        i += 1
-
     root.mainloop()
 
